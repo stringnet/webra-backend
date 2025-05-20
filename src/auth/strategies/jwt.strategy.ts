703ca28577db658b7ccf023cbacf3ed6c1a0b5c2
stringnet <135693628+stringnet@users.noreply.github.com>
2025-05-20 00:09:21 -0500

    Create jwt.strategy.ts

---
+++ src/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,6 @@
+// src/auth/strategies/jwt.strategy.ts
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { ConfigService } from '@nestjs/config';
+import { UsersService } from '../../users/users.service'; // Ajusta la ruta si es necesario
